17485925653453bad9eb7925ccccea25dadf26cc
karan kartikeya <61800214+karankartikeya@users.noreply.github.com>
2020-12-10 16:00:54 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'XXXXXXXXXXXXXXX';
+dbPassword = 'mongodb+srv://Karan_Kartikeya:Karan123@kk7.yom6h.mongodb.net/Sparks-banking?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
